65bf5fb6c1a5eeefdb1088b04250c99d5495d93b
dcentelles <centelld@uji.es>
2017-09-25 09:43:32 +0200

    fix format

--- src/simulator/main/netsim.cpp
+++ src/simulator/main/netsim.cpp
@@ -1,72 +1,65 @@
-#include <iostream>
 #include <cstdio>
-#include  <cstdio>
-#include  <sys/types.h>
-#include  <signal.h>
+#include <cstdio>
+#include <iostream>
+#include <signal.h>
+#include <sys/types.h>
 
 #include <dccomms_ros/simulator/ROSCommsSimulator.h>
 
-//ROS
+// ROS
 #include <ros/ros.h>
-//end ROS
+// end ROS
 
 using namespace dccomms;
 using namespace dccomms_ros;
 using namespace std;
 
+static std::shared_ptr<spd::logger> Log =
+    spd::stdout_color_mt("MERBOTSCommsSimulator");
+ROSCommsSimulator *sim;
 
-static std::shared_ptr<spd::logger> Log = spd::stdout_color_mt("MERBOTSCommsSimulator");
-ROSCommsSimulator * sim;
-
-void SIGINT_handler (int sig)
-{
-        printf("Received %d signal\n",sig);
-        printf("Log messages flushed.\n");
-        exit(0);
+void SIGINT_handler(int sig) {
+  printf("Received %d signal\n", sig);
+  printf("Log messages flushed.\n");
+  exit(0);
 }
 
-void setSignals()
-{
-    if (signal(SIGINT, SIGINT_handler) == SIG_ERR) {
-         printf("SIGINT install error\n");
-         exit(1);
-    }
+void setSignals() {
+  if (signal(SIGINT, SIGINT_handler) == SIG_ERR) {
+    printf("SIGINT install error\n");
+    exit(1);
+  }
 }
 
-int main(int argc, char ** argv)
-{
-    setSignals();
+int main(int argc, char **argv) {
+  setSignals();
 
-    //// GET PARAMS
-    ros::init(argc, argv, "dccomms_netsim");
-    ros::NodeHandle nh("~");
+  //// GET PARAMS
+  ros::init(argc, argv, "dccomms_netsim");
+  ros::NodeHandle nh("~");
 
-    sim = new ROSCommsSimulator(nh);
-    sim->SetLogName ("netsim");
-    sim->LogToFile ("netsim_log");
+  sim = new ROSCommsSimulator(nh);
+  sim->SetLogName("netsim");
+  sim->LogToFile("netsim_log");
 
+  Log->set_level(spdlog::level::debug);
+  Log->flush_on(spd::level::info);
 
-    Log->set_level(spdlog::level::debug);
-    Log->flush_on(spd::level::info);
+  sim->SetTransmitPDUCb([](int linkType, dccomms::DataLinkFramePtr dlf) {
+    Log->info("Transmitting PDU");
+  });
+  sim->SetReceivePDUCb([](int linkType, dccomms::DataLinkFramePtr dlf) {
+    Log->info("PDU Received");
+  });
+  sim->SetErrorPDUCb([](int linkType, dccomms::DataLinkFramePtr dlf) {
+    Log->warn("PDU Received with errors");
+  });
 
-    sim->SetTransmitPDUCb ([](int linkType, dccomms::DataLinkFramePtr dlf){
-        Log->info("Transmitting PDU");
-    });
-    sim->SetReceivePDUCb ([](int linkType, dccomms::DataLinkFramePtr dlf){
-        Log->info("PDU Received");
-    });
-    sim->SetErrorPDUCb ([](int linkType, dccomms::DataLinkFramePtr dlf){
-        Log->warn("PDU Received with errors");
-    });
+  sim->Start();
 
-    sim->Start();
-
-    ros::Rate loop_rate(30);
-    while(ros::ok())
-    {
-        loop_rate.sleep();
-        ros::spinOnce();
-    }
-
+  ros::Rate loop_rate(30);
+  while (ros::ok()) {
+    loop_rate.sleep();
+    ros::spinOnce();
+  }
 }
-
