2d80fcc08b9dc9e870dbd7e45035816561649e4c
dcentelles <centelld@uji.es>
2017-10-04 10:16:55 +0200

    comms testing

--- testing/comms/op.cpp
+++ testing/comms/op.cpp
@@ -1,3 +1,37 @@
-#include <cpplogging/Logger.h>
+#include <cpplogging/cpplogging.h>
+#include <testing/comms/utils.hpp>
 
-int main(int argc, char **argv) {}
+// ROS
+#include "ros/ros.h"
+// end ROS
+
+using namespace cpplogging;
+
+LoggerPtr Log;
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "test_op");
+  ros::NodeHandle nh("~");
+
+  Log = CreateLogger("OperatorMain");
+  Log->SetLogLevel(debug);
+
+  std::string logPrefix;
+  bool logToFileEnabled;
+  if (!nh.getParam("logPrefix", logPrefix)) {
+    Log->Error("Failed to get param 'logPrefix'");
+    return 1;
+  } else {
+    if (logPrefix == "") {
+      Log->Info("Do not log to file");
+      logToFileEnabled = false;
+    } else {
+      Log->Info("Log prefix: {}", logPrefix);
+      logToFileEnabled = true;
+    }
+  }
+
+  if (logToFileEnabled) {
+    Log->LogToFile(logPrefix + "_main");
+  }
+}
---
+++ testing/comms/utils.hpp
@@ -0,0 +1,6 @@
+#ifndef TESTING_COMMS_UTILS_HPP
+#define TESTING_COMMS_UTILS_HPP
+
+#include <dccomms/Packet.h>
+
+#endif
