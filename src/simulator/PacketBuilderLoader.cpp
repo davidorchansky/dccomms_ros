f29b4b745d852e35c7eb86619bcd360bb134ed03
dcentelles <centelld@uji.es>
2018-03-09 12:24:06 +0100

    add custom exception

--- src/simulator/PacketBuilderLoader.cpp
+++ src/simulator/PacketBuilderLoader.cpp
@@ -4,19 +4,27 @@ namespace dccomms_ros {
 
 // class_loader::MultiLibraryClassLoader PacketBuilderLoader::_loader(true);
 
+PacketBuilderLoaderException::PacketBuilderLoaderException(std::string msg, int cod) {
+  message = msg;
+  code = cod;
+}
+
 dccomms::PacketBuilderPtr
 PacketBuilderLoader::LoadPacketBuilder(const std::string &libName,
                                        const std::string className) {
   class_loader::ClassLoader loader(libName);
-  //_loader.loadLibrary(libName);
+  dccomms::IPacketBuilder *pb;
   std::vector<std::string> classes =
       loader.getAvailableClasses<dccomms::IPacketBuilder>();
   for (unsigned int c = 0; c < classes.size(); ++c) {
     if (classes[c] == className) {
-      dccomms::IPacketBuilder *pb =
-          loader.createUnmanagedInstance<dccomms::IPacketBuilder>(classes[c]);
-      return dccomms::PacketBuilderPtr(pb);
+      pb = loader.createUnmanagedInstance<dccomms::IPacketBuilder>(classes[c]);
+      break;
     }
   }
+  if(!pb)
+    throw PacketBuilderLoaderException("no '"+className+"' class found", PB_LOADER_EXCEPTION_NOCLASSFOUND);
+
+  return dccomms::PacketBuilderPtr(pb);
 }
 }
